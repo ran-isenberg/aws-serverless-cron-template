9fbfc571e5c4217fa7b7571181c56a453156e78d
Ran Isenberg <ran.isenberg@ranthebuilder.cloud>
2023-04-25 11:32:54 +0300

    refactor: shorten code

--- cdk/my_service/service_stack.py
+++ cdk/my_service/service_stack.py
@@ -1,8 +1,10 @@
 import os
 from pathlib import Path
 
-from aws_cdk import RemovalPolicy, Stack, Tags
+from aws_cdk import Duration, RemovalPolicy, Stack, Tags
+from aws_cdk import aws_iam as iam
 from aws_cdk import aws_lambda as _lambda
+from aws_cdk import aws_logs
 from aws_cdk.aws_lambda_python_alpha import PythonLayerVersion
 from constructs import Construct
 from git import Repo
@@ -22,6 +24,7 @@ def get_username() -> str:
 
 def get_stack_name() -> str:
     repo = Repo(Path.cwd())
+    # deepcode ignore NoHardcodedCredentials
     username = get_username()
     try:
         return f'{username}-{repo.active_branch}-{constants.SERVICE_NAME}'
@@ -36,10 +39,10 @@ class ServiceStack(Stack):
         self.id_ = id
         Tags.of(self).add('schedule', constants.SERVICE_NAME)
         Tags.of(self).add('owner', get_username())
-        lambda_layer = self._build_common_layer()
-        self.lambdas_rule = LambdaCronRuleConstruct(self, self.shorten_construct_id('lambda_rule'), lambda_layer)
+        self.target_lambda = self._create_target_lambda()
+        self.lambdas_rule = LambdaCronRuleConstruct(self, self.shorten_construct_id('lambda_rule'), self.target_lambda)
         self.state_func_rule = StepFuncCronRuleConstruct(self, self.shorten_construct_id('step_func_rule'))
-        self.scheduler = SchedulerCronConstruct(self, self.shorten_construct_id('scheduler_lambda'), lambda_layer)
+        self.scheduler = SchedulerCronConstruct(self, self.shorten_construct_id('scheduler_lambda'), self.target_lambda)
 
     def shorten_construct_id(self, construct_name: str) -> str:
         return f'{self.id_}_{construct_name}'[0:64]
@@ -51,4 +54,31 @@ class ServiceStack(Stack):
             entry=constants.COMMON_LAYER_BUILD_FOLDER,
             compatible_runtimes=[_lambda.Runtime.PYTHON_3_10],
             removal_policy=RemovalPolicy.DESTROY,
+        )
+
+    def _create_target_lambda(self) -> _lambda.Function:
+        lambda_layer = self._build_common_layer()
+        role = iam.Role(
+            self,
+            f'{self.id_}my_role',
+            assumed_by=iam.ServicePrincipal('lambda.amazonaws.com'),
+            managed_policies=[iam.ManagedPolicy.from_aws_managed_policy_name(managed_policy_name=('service-role/AWSLambdaBasicExecutionRole'))],
+        )
+        return _lambda.Function(
+            self,
+            f'{self.id_}SchedulerJob',
+            runtime=_lambda.Runtime.PYTHON_3_10,
+            code=_lambda.Code.from_asset('.build/lambdas/'),
+            handler='service.handlers.scheduler_func.start_cron_job',
+            environment={
+                'POWERTOOLS_SERVICE_NAME': 'cron',  # for logger, tracer and metrics
+                'LOG_LEVEL': 'DEBUG',  # for logger
+            },
+            tracing=_lambda.Tracing.ACTIVE,
+            retry_attempts=0,
+            timeout=Duration.minutes(10),
+            memory_size=128,
+            layers=[lambda_layer],
+            role=role,
+            log_retention=aws_logs.RetentionDays.ONE_DAY,
         )
--- cdk/my_service/stepfunc_cron_rules.py
+++ cdk/my_service/stepfunc_cron_rules.py
@@ -7,7 +7,7 @@ class StepFuncCronRuleConstruct(Construct):
 
     def __init__(self, scope: Construct, id_: str) -> None:
         super().__init__(scope, id_)
-
+        self.id_ = id_
         self.state_machine: sfn.StateMachine = self._create_state_machine()
         self.cron_rule: aws_events.Rule = self._create_cron_with_rule(self.state_machine)
 
@@ -15,7 +15,7 @@ class StepFuncCronRuleConstruct(Construct):
         # Create a state machine with a state that waits for 10 seconds and succeeds
         return sfn.StateMachine(
             self,
-            'MyStateMachine',
+            f'{self.id_}MyStateMachine',
             definition=sfn.Wait(
                 self,
                 'MyState',
@@ -28,13 +28,13 @@ class StepFuncCronRuleConstruct(Construct):
         # between Monday to Friday at 6 PM UTC time
         return aws_events.Rule(
             self,
-            'MyDailyCronRule',
+            f'{self.id_}MyDailyCronRule',
             schedule=aws_events.Schedule.cron(
                 minute='0',
                 hour='18',
                 month='*',
                 week_day='MON-FRI',
             ),
-            rule_name='MyDailyCronRule',
+            rule_name=f'{self.id_}MyDailyCronRule',
             targets=[aws_events_targets.SfnStateMachine(machine=target_state_machine)],
         )
